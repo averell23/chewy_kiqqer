ea6dc47e16e4c59c7bb4ef018a410f11ccaf8a5c
Daniel Hahn <dha@betterplace.org>
2014-07-28 18:37:35 +0200

    Use sidekiq locking

--- chewy_kiqqer.gemspec
+++ chewy_kiqqer.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'sidekiq'
   spec.add_dependency 'chewy'
+  spec.add_dependency 'sidekiq-lock'
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/chewy_kiqqer/worker.rb
+++ lib/chewy_kiqqer/worker.rb
@@ -1,8 +1,16 @@
 require 'sidekiq'
+require 'sidekiq-lock'
 
 module ChewyKiqqer
   class Worker
     include Sidekiq::Worker
+    include Sidekiq::Lock::Worker
+
+    sidekiq_options lock: {
+      timeout: 3000,
+      name:    ->(index_name, ids) { "lock:chewy_kiqqer:#{ids}-#{[ids].flatten.sort.join('-')}" }
+    }
+
 
     def perform(index_name, ids)
       Chewy.derive_type(index_name).import ids
