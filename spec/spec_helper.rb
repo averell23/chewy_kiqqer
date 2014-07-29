a035e15c4da74826818cb8427acf301f81946c46
Daniel Hahn <dha@betterplace.org>
2014-07-29 15:19:59 +0200

    Change coverage setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 require 'chewy_kiqqer'
 require 'chewy'
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
