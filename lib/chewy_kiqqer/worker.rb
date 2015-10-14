66da9fdbfa04acce360f9c2c67dcdbcedf301deb
Daniel Hahn <dha@betterplace.org>
2015-10-14 18:11:32 +0200

    Redis lock avoids deprecation
    Newer redis_namespace implementations do not 
    allow #lock to be passed directly to the underlying
    implementation.
    
    This should fix it by always using the redis
    client directly for locking.

--- lib/chewy_kiqqer/worker.rb
+++ lib/chewy_kiqqer/worker.rb
@@ -1,11 +1,11 @@
 require 'sidekiq'
+require 'redis-namespace'
 require 'redis-lock'
 
 module ChewyKiqqer
   class Worker
 
     include Sidekiq::Worker
-
 
     def perform(index_name, ids)
       ActiveSupport::Notifications.instrument('perform.chewy_kiqqer', index_name: index_name, ids: ids) do
@@ -15,8 +15,19 @@ module ChewyKiqqer
 
     private
 
-    def with_lock(index_name, ids)
+    def sidekiq_redis
+      # Sidekiq.redis may both be a Redis client directly, or a
+      # redis namespace. Since namespaces don't allow for passing through
+      # the lock behaviour any more, we want the "real" connection in
+      # any case
       Sidekiq.redis do |redis|
+        real_redis = redis.is_a?(Redis::Namespace) ? redis.redis : redis
+        yield real_redis
+      end
+    end
+
+    def with_lock(index_name, ids)
+      sidekiq_redis do |redis|
         lock_name = "chewy-kiqqer:#{ChewyKiqqer.locking_scope}:#{index_name}-#{ids.join('-')}"
         redis.lock(lock_name, life: 60, acquire: 5) { index(index_name, ids) }
       end
